9bdbb7233eaa98c015b45ce786bedf9f44063fde
audunel <eltvik94@gmail.com>
2014-09-16 02:19:29 +0200

    Added support for simpsons method

--- TMA4135/calculus.py
+++ TMA4135/calculus.py
@@ -16,3 +16,18 @@ def secant(x0, x1, f, epsilon=0.001, n=50):
         x1 = x1 - f(x1) * (x1 - x0) / (f(x1) - f(x0))
         x0 = x_temp
     print('Maximum number of iterations exceeded! Terminated at solution x = ', x1)
+
+
+def simpson(f, a, b, m):
+    '''
+    Given a function f  and endpoints a and b, approximates the integral of f from a to b using 2m steps
+    '''
+    n = 2*m
+    h = (b - a)/n
+    s = f(a) + f(b)
+    for i in xrange(1, n, 2):
+        s += 4 * f(a + i*h)
+    for i in xrange(2, n - 1, 2):
+        s += 2 * f(a + i*h)
+
+    return (h*s)/3
